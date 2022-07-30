c7c5aba2f93a3382c2941839bd7a0cf16d4fcaa9
Andrealavi <andrealavino1410.2003@gmail.com>
2022-07-30 17:42:46 +0200

    Made some changes in ./EnkkHomework/SecondHomework/hw3.py to better fit the original homework assignment

--- EnkkHomework/SecondHomework/hw3.py
+++ EnkkHomework/SecondHomework/hw3.py
@@ -1,6 +1,7 @@
 # Assignment: starting from ../FirstHomework/hw1.py, create a command line interface which allows to save 10 users' data.
 # Data have to be saved into a dictionary and then into a list, which contains all of them.
 # The script has to refuse the insertion if the nickname is already used by another user.
+# After insertion, the script has to calculate male and female percentage and users' max, min and average age.
 # Plus: I'll use a database to save data.
 
 import mysql.connector
@@ -8,6 +9,7 @@ import os
 import dotenv
 from colored import fg, bg
 import re
+import statistics
 
 dotenv.load_dotenv()
 
@@ -21,10 +23,10 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 
-def databaseInsertion(name, surname, nickname, age):
+def databaseInsertion(name, surname, gender, nickname, age):
 
-    statement = 'INSERT INTO Users (nome, cognome, nickname, eta) VALUES (%s, %s, %s, %s)'
-    values = (name, surname, nickname, age)
+    statement = 'INSERT INTO Users (nome, cognome, sesso, nickname, eta) VALUES (%s, %s, %s, %s, %s)'
+    values = (name, surname, gender, nickname, age)
     mycursor.execute(statement, values)
     mydb.commit()
 
@@ -47,8 +49,44 @@ def nicknameControl(nickname):
     return False
 
 
+def getUsersAge():
+    usersAge = []
+
+    users = dataRetrieving('Users')
+
+    for user in users:
+        usersAge.append(user[5])
+
+    return usersAge
+
+
+def getUsersGender():
+    usersGender = []
+
+    users = dataRetrieving('Users')
+
+    for user in users:
+        usersGender.append(user[3])
+
+    return usersGender
+
+
+def getGenderPercentage(genderOfInterest):
+    genders = getUsersGender()
+
+    nGenderOfInterest = genders.count(genderOfInterest)
+
+    return ((100*nGenderOfInterest)/len(genders))
+
+
 def dataInput(datumName, expectedType):
-    datum = input("Insert your {} here:".format(datumName))
+    if datumName == 'gender':
+        print("To indicate the gender just write M (male) or F (female)")
+
+    datum = input("Insert your {} here: ".format(datumName))
+
+    if expectedType == 'gender':
+        datum = datum.upper()
 
     if expectedType == 'string':
         regex = "\d"
@@ -70,14 +108,25 @@ def dataInput(datumName, expectedType):
     return datum
 
 
-for i in range(10):
+for i in range(5):
     name = dataInput('name', 'string')
     surname = dataInput('surname', 'string')
+    gender = dataInput('gender', 'string')
     nickname = dataInput('nickname', '')
     age = int(dataInput('age', 'int'))
 
-    databaseInsertion(name, surname, nickname, age)
+    databaseInsertion(name, surname, gender, nickname, age)
 
     print("\n Thank you for your registration! It was a success!!! \n\n\n")
+
+usersAge = getUsersAge()
+
+print("Now it's time for a bit of statistics \n\n\n")
+print("The minimum age of our users is: ", min(usersAge))
+print("The maximum age of our users is: ", max(usersAge))
+print("The average age of our users is: ", statistics.fmean(usersAge))
+
+print("\n\n Talking about genders, the percentage of male is {}%, meanwhile the percentage of female is {}%".format(
+    getGenderPercentage('M'), getGenderPercentage('F')))
 
 mydb.close()
